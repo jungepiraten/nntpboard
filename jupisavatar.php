60b3401c194f311d7bd90dc6825f3b0500cbbc5a
prauscher <prauscher@prauscher.homeip.net>
2010-10-23 00:37:51 +0200

    Cache fuer avatare eingebaut

--- jupisavatar.php
+++ jupisavatar.php
@@ -1,20 +1,75 @@
 <?php
 
-$blacklist = array("Politischer Kompass.svg", "Edit add.svg");
+define("CACHEPERIOD", 3 * 24 * 60 * 60);
+define("THUMBWIDTH", 100);
+define("THUMBHEIGHT", 100);
 
 $name = stripslashes($_REQUEST["name"]);
+$cachename = "avatarcache/" . md5($name);
 
-$link = "http://wiki.junge-piraten.de/w/api.php?action=query&prop=images&&format=xml&titles=Benutzer:" . urlencode($name);
-$xml = file_get_contents($link);
+// Wenn unser Cachebild nicht zu alt ist, benutze es
+if (file_exists($cachename . ".png") && filemtime($cachename . ".png") > time() - CACHEPERIOD) {
+	header("Content-Type: image/png");
+	readfile($cachename . ".png");
+	exit;
+}
 
-preg_match_all('#<im\s+ns="6"\s+title="Datei:(.*)"[\s/]*>#Ui', $xml, $matches, PREG_SET_ORDER);
+// Funktion zum Laden eines Bildes aus der Wikiseite
+function getImage($page) {
+	$link = "http://wiki.junge-piraten.de/w/api.php?action=query&prop=images&&format=xml&titles=" . urlencode($page);
+	$blacklist = array("Politischer Kompass.svg", "Edit add.svg", "Zebramitstern.png");
 
-foreach ($matches as $match) {
-	$file = $match[1];
-	if (!in_array($file, $blacklist)) {
-		header("Location: http://wiki.junge-piraten.de/wiki/Spezial:Dateipfad/" . urlencode(str_replace(" ", "_", $file)));
-		exit;
+	$xml = file_get_contents($link);
+
+	preg_match_all('#<im\s+ns="6"\s+title="Datei:(.*(\.jpg|\.jpeg|\.gif|\.png))"[\s/]*>#Ui', $xml, $matches, PREG_SET_ORDER);
+
+	foreach ($matches as $match) {
+		$file = $match[1];
+		if (!in_array($file, $blacklist)) {
+			return "http://wiki.junge-piraten.de/wiki/Spezial:Dateipfad/" . urlencode(str_replace(" ", "_", $file));
+		}
 	}
+
+	return null;
+}
+
+// Ladereihenfolge: Zuerst Benutzer/Avatar - falls dieses nicht existiert, probiere die Regulaere Benutzerseite
+$image = getImage("Benutzer:" . ucfirst($name) . "/Avatar");
+if ($image === null) {
+	$image = getImage("Benutzer:" . ucfirst($name));
+}
+
+if ($image !== null) {
+	if (substr($image, -5) == ".jpeg" || substr($image, -4) == ".jpg") {
+		$img = ImageCreateFromJPEG($image);
+	} else if (substr($image, -4) == ".gif") {
+		$img = ImageCreateFromGIF($image);
+	} else if (substr($image, -4) == ".png") {
+		$img = ImageCreateFromPNG($image);
+	}
+	if (!is_resource($img)) {
+		die("Fail!");
+	}
+	$origw = ImageSx($img);
+	$origh = ImageSy($img);
+	if ($origw > THUMBWIDTH || $origh > THUMBHEIGHT) {
+		if ($origw > $origh) {
+			$thumbw = THUMBWIDTH;
+			$thumbh = $origh * (THUMBWIDTH / $origw);
+		} else if ($origw < $origh) {
+			$thumbw = $origw * (THUMBHEIGHT / $origh);
+			$thumbh = THUMBHEIGHT;
+		}
+		$thumb = ImageCreateTrueColor($thumbw, $thumbh);
+		ImageCopyResampled($thumb, $img, 0, 0, 0, 0, $thumbw, $thumbh, $origw, $origh);
+	} else {
+		$thumb = $img;
+	}
+	ImagePNG($thumb, $cachename . ".png");
+	ImageDestroy($thumb);
+	header("Location: image/png");
+	readfile($cachename . ".png");
+	exit;
 }
 
 header("Content-Type: image/png");
