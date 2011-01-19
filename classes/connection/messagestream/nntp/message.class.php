fbce2f47cdbf06a8ea1cc95f618188c078084975
Patrick Rauscher <prauscher@prauscher.homeip.net>
2011-01-19 20:42:55 +0100

    bugfix bei komischen references-headern

--- classes/connection/messagestream/nntp/message.class.php
+++ classes/connection/messagestream/nntp/message.class.php
@@ -118,8 +118,10 @@ class NNTPMessage {
 		// References (per Default als neuer Thread)
 		$parentid = null;
 		if ($this->getHeader()->has("References") && trim($this->getHeader()->get("References")->getValue($charset)) != "") {
-			$references = explode(" ", $this->getHeader()->get("References")->getValue($charset));
-			$parentid = array_pop($references);
+			do {
+				$references = explode(" ", $this->getHeader()->get("References")->getValue($charset));
+				$parentid = array_pop($references);
+			} while (!$connection->hasMessage($parentid));
 		}
 
 		try {
