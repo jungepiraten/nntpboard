f0865af744cb480d80b36facdaf478a695b8bdb1
Patrick Rauscher <prauscher@ohai.su>
2012-09-01 14:34:16 +0000

    fixed callorder of FileCacheBoards

--- classes/board/filecachedimap.class.php
+++ classes/board/filecachedimap.class.php
@@ -4,8 +4,8 @@ require_once(dirname(__FILE__) . "/cachedimap.class.php");
 require_once(dirname(__FILE__) . "/../connection/itemcache/file.class.php");
 
 class FileCachedIMAPBoard extends CachedIMAPBoard {
-	public function __construct($boardid, $parentid, $name, $desc, $host, $loginusername, $loginpassword, $folder, $anonMayPost, $authMayPost, $isModerated) {
-		parent::__construct($boardid, $parentid, $name, $desc, $host, $loginusername, $loginpassword, $folder, $anonMayPost, $authMayPost, $isModerated);
+	public function __construct($boardid, $parentid, $name, $desc, $anonMayPost, $authMayPost, $isModerated, $host, $loginusername, $loginpassword, $folder) {
+		parent::__construct($boardid, $parentid, $name, $desc, $anonMayPost, $authMayPost, $isModerated, $host, $loginusername, $loginpassword, $folder);
 	}
 
 	public function getConnection() {
