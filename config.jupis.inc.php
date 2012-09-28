c32be222d7dd94d9b7442bf6d45bbef8afedf38f
Patrick Rauscher <prauscher@ohai.su>
2012-09-28 07:22:39 +0000

    add verwaltung-public

--- config.jupis.inc.php
+++ config.jupis.inc.php
@@ -28,6 +28,7 @@ class JuPiConfig extends DefaultConfig {
 		$this->addGenericBoard(4, null, "announce", "announce", null, "Ankündigungen", "Allgemeine Ankündigungen");
 		$this->addGenericBoard(2, null, "misc", "aktive", null, "Allgemeines", "Globale Themen der Jungen Piraten");
 		$this->addGenericBoard(7, null, "board.public", "vorstand-public", null, "Vorstand", "Öffentliche Vorstandsliste");
+		$this->addGenericBoard(8, null, "verwaltung.public", "verwaltung-public", null, "Verwaltung", "Öffentliche Verwaltungsliste");
 
 		$this->addOrgaStruktur(300, null);
 
