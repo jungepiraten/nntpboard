638f31e575b11c96c8c297c9b5c3f554c1079516
Patrick Rauscher <prauscher@ohai.su>
2013-04-25 12:30:47 +0000

    add script to index a given database

---
+++ bin/indexer.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(dirname(__FILE__)."/config.inc.php");
+
+$indexer = $config->getIndexer();
+
+foreach ($config->getBoardIDs() as $boardid) {
+	$cache = $config->getBoard($boardid)->getConnection();
+
+	// Benutze keine Authentifikation
+	$cache->open(NULL);
+
+	$group = $cache->getGroup();
+	foreach ($group->getMessageIDs() as $messageid) {
+		$indexer->addMessage($boardid, $messageid);
+	}
+
+	$cache->close();
+}
+
+?>
