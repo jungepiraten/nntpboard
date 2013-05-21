ab391287992fe60f8bd0d307ac4adde8c07e6c85
Patrick Rauscher <prauscher@ohai.su>
2013-05-21 07:34:48 +0000

    improve exceptionhandling

--- viewthread.php
+++ viewthread.php
@@ -8,73 +8,75 @@ $boardid = stripslashes($_REQUEST["boardid"]);
 $threadid = isset($_REQUEST["threadid"]) ? $config->decodeMessageID(stripslashes($_REQUEST["threadid"])) : null;
 $messageid = isset($_REQUEST["messageid"]) ? $config->decodeMessageID(stripslashes($_REQUEST["messageid"])) : null;
 
-$board = $config->getBoard($boardid);
-if ($board === null) {
-	$template->viewexception(new Exception("Board nicht gefunden!"));
-}
-
-if (!$board->mayRead($session->getAuth())) {
-	$template->viewexception(new Exception("Keine Berechtigung!"));
-}
-
-$connection = $board->getConnection();
-if ($connection === null) {
-	$template->viewexception(new Exception("Board enthaelt keine Group!"));
-}
-
-/* Thread laden */
-// Sobald die Verbindung geoeffnet ist, beginnen wir einen Kritischen Abschnitt!
-$connection->open($session->getAuth());
-$group = $connection->getGroup();
-$connection->close();
-
-if ($threadid === null && $messageid !== null) {
-	$message = $group->getMessage($messageid);
-	if (!($message instanceof Message)) {
-		$template->viewexception(new Exception("Message konnte nicht zugeordnet werden."));
+try {
+	$board = $config->getBoard($boardid);
+	if ($board === null) {
+		throw new Exception("Board nicht gefunden!");
 	}
-	$thread = $group->getThread($messageid);
-	$template->viewmessage($board, $thread, $message, $board->mayPost($session->getAuth()), $board->mayAcknowledge($session->getAuth()));
-}
 
-$thread = $group->getThread($threadid);
-if (!($thread instanceof Thread)) {
-	$template->viewexception(new Exception("Thread nicht gefunden!"));
-}
+	if (!$board->mayRead($session->getAuth())) {
+		throw new Exception("Keine Berechtigung!");
+	}
 
-// Erzwinge mindestens eine Seite
-$pages = max(ceil($thread->getMessageCount() / $config->getMessagesPerPage()), 1);
-$page = 0;
-if (isset($_REQUEST["page"])) {
-	$page = intval($_REQUEST["page"]);
-}
-// Vorsichtshalber erlauben wir nur Seiten, auf dennen auch Nachrichten stehen
-if ($page < 0 || $page > $pages) {
+	$connection = $board->getConnection();
+	if ($connection === null) {
+		throw new Exception("Board enthaelt keine Group!");
+	}
+
+	/* Thread laden */
+	// Sobald die Verbindung geoeffnet ist, beginnen wir einen Kritischen Abschnitt!
+	$connection->open($session->getAuth());
+	$group = $connection->getGroup();
+	$connection->close();
+
+	if ($threadid === null && $messageid !== null) {
+		$message = $group->getMessage($messageid);
+		if (!($message instanceof Message)) {
+			throw new Exception("Message konnte nicht zugeordnet werden.");
+		}
+		$thread = $group->getThread($messageid);
+		$template->viewmessage($board, $thread, $message, $board->mayPost($session->getAuth()), $board->mayAcknowledge($session->getAuth()));
+	}
+
+	$thread = $group->getThread($threadid);
+
+	// Erzwinge mindestens eine Seite
+	$pages = max(ceil($thread->getMessageCount() / $config->getMessagesPerPage()), 1);
 	$page = 0;
-}
-
-// Nachrichten laden
-$messageids = array_slice($thread->getMessageIDs(), $page * $config->getMessagesPerPage(), $config->getMessagesPerPage());
-$messages = array();
-
-foreach ($messageids AS $messageid) {
-	$message = array();
-	$message["message"] = $group->getMessage($messageid);
-	$message["acknowledges"] = array();
-	$acknowledgeids = $group->getAcknowledgeMessageIDs($messageid);
-	foreach ($acknowledgeids as $acknowledgeid) {
-		$message["acknowledges"][$acknowledgeid] = $group->getAcknowledge($acknowledgeid);
+	if (isset($_REQUEST["page"])) {
+		$page = intval($_REQUEST["page"]);
 	}
-	$messages[] = $message;
-}
-// $message will now contain the last message on that page
+	// Vorsichtshalber erlauben wir nur Seiten, auf dennen auch Nachrichten stehen
+	if ($page < 0 || $page > $pages) {
+		$page = 0;
+	}
 
-$connection->close();
-if (!is_array($messages) || count($messages) < 1) {
-	$template->viewexception(new Exception("Thread ungueltig!"));
+	// Nachrichten laden
+	$messageids = array_slice($thread->getMessageIDs(), $page * $config->getMessagesPerPage(), $config->getMessagesPerPage());
+	$messages = array();
+
+	foreach ($messageids AS $messageid) {
+		$message = array();
+		$message["message"] = $group->getMessage($messageid);
+		$message["acknowledges"] = array();
+		$acknowledgeids = $group->getAcknowledgeMessageIDs($messageid);
+		foreach ($acknowledgeids as $acknowledgeid) {
+			$message["acknowledges"][$acknowledgeid] = $group->getAcknowledge($acknowledgeid);
+		}
+		$messages[] = $message;
+	}
+	// $message will now contain the last message on that page
+
+	$connection->close();
+	if (!is_array($messages) || count($messages) < 1) {
+		$template->viewexception(new Exception("Thread ungueltig!"));
+	}
+
+	// order is important: the template will check if the message is unread
+	$template->viewthread($board, $thread, $page, $pages, $messages, $board->mayPost($session->getAuth()), $board->mayAcknowledge($session->getAuth()));
+	$session->getAuth()->markReadThread($thread, $message["message"]);
+} catch (Exception $e) {
+	$template->viewexception($e);
 }
 
-// order is important: the template will check if the message is unread
-$template->viewthread($board, $thread, $page, $pages, $messages, $board->mayPost($session->getAuth()), $board->mayAcknowledge($session->getAuth()));
-$session->getAuth()->markReadThread($thread, $message["message"]);
 ?>
