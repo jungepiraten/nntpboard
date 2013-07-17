01ceda3646c27813d5e54234f78bd0cca8599cb7
Patrick Rauscher <prauscher@ohai.su>
2013-07-17 12:02:29 +0000

    move to rfc5322-superclass

---
+++ classes/connection/messagestream/rfc5322.class.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once(dirname(__FILE__)."/../messagestream.class.php");
+require_once(dirname(__FILE__)."/rfc5322/header.class.php");
+require_once(dirname(__FILE__)."/rfc5322/message.class.php");
+
+abstract class AbstractRFC5322Connection extends AbstractMessageStreamConnection {
+	public function getMessage($msgid) {
+		$rfcmessage = $this->getRFC5322Message($msgid);
+		return $rfcmessage->getObject($this);
+	}
+
+	abstract protected function getRFC5322Message($msgid);
+
+	public function postMessage($message) {
+		return $this->post( RFC5322Message::parseObject($this, $message) );
+	}
+
+	public function postAcknowledge($ack, $message) {
+		return $this->post( RFC5322Message::parseAcknowledgeObject($this, $ack, $message) );
+	}
+
+	public function postCancel($cancel, $message) {
+		return $this->post( RFC5322Message::parseCancelObject($this, $cancel, $message) );
+	}
+
+	abstract protected function post($rfc5322msg);
+}
+
+?>
