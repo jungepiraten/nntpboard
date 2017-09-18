jungepiraten/nntpboard

commit b65130af62816e2fa56db7605b5a122d80b0ff83

move include for unserialize to part where unserialize actually gets called

classes/connection/cache/redis.class.php
@@ -2,6 +2,17 @@
 
 require_once(dirname(__FILE__) . "/keyvalue.class.php");
 require_once(dirname(__FILE__) . "/redis/RedisServer.php");
+/* Die Klassen müssen vor dem unserialize eingebunden sein, da PHP sonst
+ * incomplete Objekte erstellt.
+ * vgl. http://mrfoo.de/archiv/120-The-script-tried-to-execute-a-method-or-access-a-property-of-an-incomplete-object.html
+ **/
+require_once(dirname(__FILE__)."/../address.class.php");
+require_once(dirname(__FILE__)."/../thread.class.php");
+require_once(dirname(__FILE__)."/../message.class.php");
+require_once(dirname(__FILE__)."/../acknowledge.class.php");
+require_once(dirname(__FILE__)."/../cancel.class.php");
+require_once(dirname(__FILE__)."/../exceptions/thread.exception.php");
+require_once(dirname(__FILE__)."/../exceptions/message.exception.php");
 
 class RedisCacheConnection extends KeyValueCacheConnection {
 	private $rediscache;

classes/connection/cache.class.php
@@ -3,17 +3,6 @@
 require_once(dirname(__FILE__)."/../connection.class.php");
 require_once(dirname(__FILE__)."/../boardindexer.class.php");
 require_once(dirname(__FILE__)."/../group/dynamic.class.php");
-/* Die Klassen müssen vor dem unserialize eingebunden sein, da PHP sonst
- * incomplete Objekte erstellt.
- * vgl. http://mrfoo.de/archiv/120-The-script-tried-to-execute-a-method-or-access-a-property-of-an-incomplete-object.html
- **/
-require_once(dirname(__FILE__)."/../address.class.php");
-require_once(dirname(__FILE__)."/../thread.class.php");
-require_once(dirname(__FILE__)."/../message.class.php");
-require_once(dirname(__FILE__)."/../acknowledge.class.php");
-require_once(dirname(__FILE__)."/../cancel.class.php");
-require_once(dirname(__FILE__)."/../exceptions/thread.exception.php");
-require_once(dirname(__FILE__)."/../exceptions/message.exception.php");
 
 interface CacheConnection {
 	public function getMessageQueue($queueid);

commit 64baca837271a43d9baed40697c7639ee2fba5fc

correct paths broken by b65130af

classes/connection/cache/redis.class.php
@@ -6,13 +6,13 @@ require_once(dirname(__FILE__) . "/redis/RedisServer.php");
  * incomplete Objekte erstellt.
  * vgl. http://mrfoo.de/archiv/120-The-script-tried-to-execute-a-method-or-access-a-property-of-an-incomplete-object.html
  **/
-require_once(dirname(__FILE__)."/../address.class.php");
-require_once(dirname(__FILE__)."/../thread.class.php");
-require_once(dirname(__FILE__)."/../message.class.php");
-require_once(dirname(__FILE__)."/../acknowledge.class.php");
-require_once(dirname(__FILE__)."/../cancel.class.php");
-require_once(dirname(__FILE__)."/../exceptions/thread.exception.php");
-require_once(dirname(__FILE__)."/../exceptions/message.exception.php");
+require_once(dirname(__FILE__)."/../../address.class.php");
+require_once(dirname(__FILE__)."/../../thread.class.php");
+require_once(dirname(__FILE__)."/../../message.class.php");
+require_once(dirname(__FILE__)."/../../acknowledge.class.php");
+require_once(dirname(__FILE__)."/../../cancel.class.php");
+require_once(dirname(__FILE__)."/../../exceptions/thread.exception.php");
+require_once(dirname(__FILE__)."/../../exceptions/message.exception.php");
 
 class RedisCacheConnection extends KeyValueCacheConnection {
 	private $rediscache;